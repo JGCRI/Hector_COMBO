a62f2fcfd7494885a94041be0f6ed9e8498c062d
Corinne Hartin <corinne.hartin@pnnl.gov>
2019-09-06 14:24:29 -0400

    reorganizing repo

--- 2012.07.09.COMBO/2012.07.09.COMBO_PlosOne/Temps_hist/temps_read99.m
+++
@@ -1,9 +0,0 @@
-% open temperature baseline files
-%
-% Assumes files are simply two columns: month and temperature (no headers)
-
-fid = fopen('R1 hi5b.txt');
-data = [data; fscanf(fid, '%*s %f')];
-fclose(fid);
-
-temps = baseline;
